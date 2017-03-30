54a8ccc34de02fc8a291899d8dc28920c162f8e2
Valeria <protsan.valeriya@gmail.com>
2017-03-30 12:26:04 -0400

    all styles applied

--- gulpfile.js
+++ gulpfile.js
@@ -89,7 +89,7 @@ gulp.task('clean:dist', function() {
 });
 
 gulp.task('default', function(callback) {
-  runSequence(['browserSync','sass'], 'watch',
+  runSequence(['sass', 'browserSync'], 'watch',
     callback
   )
 })
